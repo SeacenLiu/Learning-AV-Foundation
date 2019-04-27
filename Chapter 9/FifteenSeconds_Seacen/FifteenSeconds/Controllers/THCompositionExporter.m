7fe1e485771b538b547bd364387fb5ffcf628069
SeacenLiu <1016667972@qq.com>
2019-04-27 17:41:16 +0800

    15 Seconds 初步浏览，里面还有很多需要学习

---
+++ Chapter 9/FifteenSeconds_Seacen/FifteenSeconds/Controllers/THCompositionExporter.m
@@ -0,0 +1,117 @@
+//
+//  MIT License
+//
+//  Copyright (c) 2014 Bob McCune http://bobmccune.com/
+//  Copyright (c) 2014 TapHarmonic, LLC http://tapharmonic.com/
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+#import "THCompositionExporter.h"
+#import "UIAlertView+THAdditions.h"
+#import <AssetsLibrary/AssetsLibrary.h>
+
+@interface THCompositionExporter ()
+@property (strong, nonatomic) id <THComposition> composition;
+@property (strong, nonatomic) AVAssetExportSession *exportSession;
+@end
+
+@implementation THCompositionExporter
+
+- (instancetype)initWithComposition:(id <THComposition>)composition {
+
+    self = [super init];
+    if (self) {
+        _composition = composition;
+    }
+    return self;
+}
+
+- (void)beginExport {
+    // Listing 9.9
+    self.exportSession = [self.composition makeExportable];
+    self.exportSession.outputURL = [self exportURL];
+    self.exportSession.outputFileType = AVFileTypeMPEG4;
+    
+    [self.exportSession exportAsynchronouslyWithCompletionHandler:^{
+        // Listing 9.11
+        AVAssetExportSessionStatus status = self.exportSession.status;
+        if (status == AVAssetExportSessionStatusCompleted) {
+            [self writeExportedVideoToAssetsLibrary];
+        } else {
+            [UIAlertView showAlertWithTitle:@"Export Failed" message:@"The request export failed."];
+        }
+    }];
+    
+    self.exporting = YES;
+    [self monitorExportProgress];
+}
+
+- (void)monitorExportProgress {
+    // Listing 9.10
+    double delayInseconds = 0.1;
+    int64_t delta = (int64_t)delayInseconds * NSEC_PER_SEC;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, delta), dispatch_get_main_queue(), ^{
+        AVAssetExportSessionStatus status = self.exportSession.status;
+        
+        if (status == AVAssetExportSessionStatusExporting) {
+            self.progress = self.exportSession.progress;
+            [self monitorExportProgress];
+        } else {
+            self.exporting = NO;
+        }
+    });
+}
+
+- (void)writeExportedVideoToAssetsLibrary {
+    // Listing 9.11
+    NSURL *exportURL = self.exportSession.outputURL;
+    ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+    
+    if ([library videoAtPathIsCompatibleWithSavedPhotosAlbum:exportURL]) {
+        [library writeVideoAtPathToSavedPhotosAlbum:exportURL completionBlock:^(NSURL *assetURL, NSError *error) {
+            if (error) {
+                NSString *message = @"Unable to write to photos library.";
+                [UIAlertView showAlertWithTitle:@"Write Failed" message:message];
+            }
+            
+            [[NSFileManager defaultManager] removeItemAtURL:exportURL error:nil];
+        }];
+    } else {
+        NSLog(@"Video could not be exported to the assets library.");
+    }
+}
+
+- (NSURL *)exportURL {
+    NSString *filePath = nil;
+    NSUInteger count = 0;
+    do {
+        filePath = NSTemporaryDirectory();
+        NSString *numberString = count > 0 ?
+            [NSString stringWithFormat:@"-%li", (unsigned long) count] : @"";
+        NSString *fileNameString =
+            [NSString stringWithFormat:@"Masterpiece-%@.m4v", numberString];
+        filePath = [filePath stringByAppendingPathComponent:fileNameString];
+        count++;
+    } while ([[NSFileManager defaultManager] fileExistsAtPath:filePath]);
+
+    return [NSURL fileURLWithPath:filePath];
+}
+
+@end
